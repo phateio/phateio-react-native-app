e4aab2718de296adf862efae34ad603451864a37
Shi-Ken Don <shikendon@users.noreply.github.com>
2016-09-17 05:31:29 +0800

    Add basic landing page layout

--- index.android.js
+++ index.android.js
@@ -9,23 +9,34 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
 } from 'react-native';
+import { Player } from 'react-native-audio-streaming';
 
 class phateio extends Component {
+  constructor(props) {
+    super(props);
+
+    setInterval(() => {
+      this.setState({});
+    }, 1000);
+  }
+
   render() {
+    var moment = require('moment');
+    let now_time = moment().format('LTS');
     return (
       <View style={styles.container}>
+        <Image source={{uri: 'https://i.imgur.com/8BOmHBN.jpg'}} style={styles.background} />
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to Phate Radio{'\n'}
+          {now_time}
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.android.js
+          Experimental version{'\n'}
         </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <Player url={'https://phate.io/listen'} />
       </View>
     );
   }
@@ -35,18 +46,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'stretch',
     backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    color: '#EEEEEE',
     margin: 10,
   },
   instructions: {
     textAlign: 'center',
-    color: '#333333',
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    color: '#EEEEEE',
     marginBottom: 5,
+  },
+  background: {
+    position: 'absolute',
+    top: 0,
+    bottom: 80,
+    left: 0,
+    right: 0,
   },
 });
 
